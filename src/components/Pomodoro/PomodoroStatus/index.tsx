960fba5afb3145313a918b86cf3eb83c5fa230c7
Reinny <reynsth@gmail.com>
2021-05-17 15:59:33 -0400

    Styling Pomodoro Status

--- src/components/Pomodoro/PomodoroStatus/index.tsx
+++ src/components/Pomodoro/PomodoroStatus/index.tsx
@@ -1,4 +1,5 @@
 import { Status } from "../../../types/status";
+import * as Styled from "./styles";
 
 type ComponentProps = {
 	currentStatus: Status["title"];
@@ -7,10 +8,11 @@ type ComponentProps = {
 
 const PomodoroStatus = ({ currentStatus, pomodorosCompleted }: ComponentProps) => {
 	return (
-		<div>
-			<h3>Current Status: { currentStatus }</h3>
-			<h3>Pomodoros Completed: { pomodorosCompleted }</h3>
-		</div>
+		<Styled.PomodoroStatus>
+			<h3>{ currentStatus }</h3>
+			<p role="separator">|</p>
+			<h3>{ `#${ pomodorosCompleted }` }</h3>
+		</Styled.PomodoroStatus>
 	)
 };
 
