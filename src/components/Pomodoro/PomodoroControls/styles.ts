66bd3274a00ca00f3b674dfd6ef57d2292236c9c
Reinny <reynsth@gmail.com>
2021-05-25 21:09:50 -0400

    Using the Icon component in the Pomodoro Controls

--- src/components/Pomodoro/PomodoroControls/styles.ts
+++ src/components/Pomodoro/PomodoroControls/styles.ts
@@ -4,28 +4,6 @@ const PomodoroControls = styled.div`
   display: flex;
   justify-content: space-evenly;
   align-items: center;
-
-  button {
-    width: 3rem;
-    height: 3rem;
-    border: none;
-    border-radius: 50%;
-    background-color: white;
-    cursor: pointer;
-
-	  svg {
-	  	width: 100%;
-      height: 100%;
-	  }
-
-    &[disabled] {
-      cursor: not-allowed;
-
-      svg {
-        fill: ${ (props => props.theme.light.color.highlight) }
-      }
-    }
-  }
 `;
 
 export { PomodoroControls };
