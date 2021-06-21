321daa4d5b6909c6e26628c330bacdf81d97d5f6
Reinny <reynsth@gmail.com>
2021-06-21 10:37:16 -0400

    Loading the desired theme & Wrapping the App

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,11 @@
 import { StrictMode } from "react";
 import ReactDOM from "react-dom";
-import { App } from "./App";
+import AppWrapper from "./App";
 import reportWebVitals from "./reportWebVitals";
 
 ReactDOM.render(
 	<StrictMode>
-		<App />
+		<AppWrapper />
 	</StrictMode>,
 	document.getElementById("root")
 );
